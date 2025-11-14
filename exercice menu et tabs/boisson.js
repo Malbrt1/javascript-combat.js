fb1af4ffbf8fa2d6e62839530a917a12667f6efb
Malbrt1 <maelyss.bromet@edu.devinci.fr>
2025-11-14 12:30:42 +0100

    Add files via upload

---
+++ exercice menu et tabs/boisson.js
@@ -0,0 +1,10 @@
+
+
+let infoButton = document.getElementById("infoButton")
+let menu = document.getElementById("menu")
+
+
+infoButton.addEventListener("click", function () {
+    
+    menu.classList.toggle("open")
+})
